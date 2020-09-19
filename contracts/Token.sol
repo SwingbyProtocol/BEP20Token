64d6c7f02cd81fa1e5fb5feb874883725f35beaa
syrohei <syrohei@gmail.com>
2020-09-20 05:30:16 +0900

    Fixed token contracts

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -7,9 +7,7 @@ contract Token is BEP20 {
         _name = "SWINGBY TEST Token";
         _symbol = "SWINGBY-888";
         _decimals = 18;
-        _totalSupply = 1 * 10**9 * 10**18; // 1B tokens with decimals = 18
-        _balances[msg.sender] = _totalSupply;
-
-        emit Transfer(address(0), msg.sender, _totalSupply);
+        uint256 totalSupply = 1 * 10**9 * 10**18; // 1B tokens with decimals = 18
+        mint(totalSupply);
     }
 }
--- truffle-config.js
+++ truffle-config.js
@@ -64,8 +64,8 @@ module.exports = {
     // NB: It's important to wrap the provider as a function.
     bsc_testnet: {
       provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
-      network_id: 97,       // Ropsten's id
-      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      network_id: 97,      // BSC testnet id
+      gas: 5500000,        // BSC testnet has a lower block limit than mainnet
       confirmations: 6,    // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
