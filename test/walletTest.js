6c5da6422e01ddf46d20de91fe2a1283e2db7a13
syrohei <syrohei@gmail.com>
2020-10-09 21:50:52 +0900

    Added full tests for multi-send-walelt

--- test/walletTest.js
+++ test/walletTest.js
@@ -30,49 +30,54 @@ contract('MultiSendWallet', async (accounts) => {
     it('multi transfer', async () => {
         try {
             let token = await Token.new()
+            // Defaut token decimals == 18
             let mintValue = web3.utils.toWei(new BN('1000000000'), 'ether')
             let from = accounts[0];
             let to = accounts[1];
             let to2 = accounts[2]
             let amount = web3.utils.toWei(new BN('500000000'), 'ether')
-    
+
             let balanceFrom = await token.balanceOf(from);
-    
+
             assert.equal(balanceFrom.toString(), mintValue.toString());
 
             let factory = await MultiSendWalletFactory.new()
             let deployed = await factory.deployNewWallet(from)
             let wallet = await MultiSendWallet.at(deployed.logs[0].args.wallet)
-            let owner = await wallet.owner()
-            assert.equal(owner, from);
-    
             let balanceTo = await token.balanceOf(to);
-    
-            let send1 = "0x" + web3.utils.padLeft(amount.toString('hex') + to.slice(2), 64)
-    
-            let send2 = "0x" + web3.utils.padLeft(amount.toString('hex') + to2.slice(2), 64)
+            let balanceTo2 = await token.balanceOf(to2);
+
+
+            // Sending toknes to deployed contract wallet.
+            await token.transfer(wallet.address, amount)
+
+            let sendAmount = amount.div(new BN('2'))
+
+            let send1 = "0x" + web3.utils.padLeft(sendAmount.toString('hex') + to.slice(2), 64)
+            let send2 = "0x" + web3.utils.padLeft(sendAmount.toString('hex') + to2.slice(2), 64)
             // 0x00000001158e460913d00000943Bef1Fb2F25C43Ab4a010ae835E936d1A34fE1
-    
-            // console.log(send1)
-    
+
             const txs = [
                 send1, send2
             ]
-    
-            let logs = await wallet.multiTransferERC20(txs, {
+
+            // Should be 32bytes hex if calling from golang
+            const inputDecimals = 18
+
+            let logs = await wallet.multiTransferERC20TightlyPacked(token.address, txs, inputDecimals, {
                 from: from
             });
-    
+
             // console.log(logs.logs)
-    
-            let updateBalanceFrom = await token.balanceOf(from);
+
+            let updateBalanceWallet = await token.balanceOf(wallet.address);
             let updateBalanceTo = await token.balanceOf(to);
             let updateBalanceTo2 = await token.balanceOf(to2);
-    
-    
-            assert.equal(mintValue.sub(amount).sub(amount).toString(), updateBalanceFrom.toString());
-            assert.equal(balanceTo.add(amount).toString(), updateBalanceTo.toString());
-            assert.equal(amount.toString(), updateBalanceTo2.toString());
+
+            // Wallet balance should be zero
+            assert.equal(new BN('0').toString(), updateBalanceWallet.toString());
+            assert.equal(balanceTo.add(sendAmount).toString(), updateBalanceTo.toString());
+            assert.equal(balanceTo2.add(sendAmount).toString(), updateBalanceTo2.toString());
         } catch (err) {
             console.log(err)
         }
